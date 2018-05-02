e73ec18cbc492cb21ca235b3015171a75b9a23f2
Daniel Williams <danswilliams95@gmail.com>
2018-05-01 20:43:28 -0700

    adding toolbar click listening

--- android-app/app/src/main/java/org/studyslug/www/studyslug/MainActivity.java
+++ android-app/app/src/main/java/org/studyslug/www/studyslug/MainActivity.java
@@ -1,15 +1,20 @@
 package org.studyslug.www.studyslug;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.Context;
-import android.widget.Toolbar;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_list_black_24dp);
 
+        drawerLayout = findViewById(R.id.drawer);
+
         /// add intent from main to addClasses
         Intent mainToAddClasses = new Intent(this, addClasses.class);
         startActivity(mainToAddClasses);
@@ -30,5 +37,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
