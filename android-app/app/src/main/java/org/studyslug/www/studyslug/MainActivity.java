24ef592c3d0faba808840020bbf3affe26ea21ea
Minghao <mliu40@ucsc.edu>
2018-05-02 00:08:36 -0700

    Make the drawer
    Created the drawer page

--- android-app/app/src/main/java/org/studyslug/www/studyslug/MainActivity.java
+++ android-app/app/src/main/java/org/studyslug/www/studyslug/MainActivity.java
@@ -1,10 +1,13 @@
 package org.studyslug.www.studyslug;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
         /// main activity goes here
 
         /// add intent from main to addClasses
+        button = (Button) findViewById(R.id.main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain();
+            }
+        });
+    }
 
+    public void openMain(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
