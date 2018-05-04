ac5729e2af2f9e7b7d151c73bf7220082f6bdb4a
Pete Wilcox <petercwilcox@gmail.com>
2018-05-04 15:29:37 -0700

    removing unused imports

--- android-app/app/src/main/java/org/studyslug/www/studyslug/addClasses.java
+++ android-app/app/src/main/java/org/studyslug/www/studyslug/addClasses.java
@@ -2,24 +2,13 @@ package org.studyslug.www.studyslug;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.support.v7.widget.RecyclerView;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import android.content.Context;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class addClasses extends AppCompatActivity {
