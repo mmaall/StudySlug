14abfa4ac56724ac1496e21034457510d0275962
Michael Lanthier <m2lanthier@gmail.com>
2018-05-07 18:23:33 -0700

    LoginActivity.java modified. Button exists, is pressable. App no longer crashes as well

--- android-app/app/src/main/java/org/studyslug/www/studyslug/LoginActivity.java
+++ android-app/app/src/main/java/org/studyslug/www/studyslug/LoginActivity.java
@@ -38,6 +38,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -87,7 +88,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     //private View mProgressView;
     //private View mLoginFormView;
     private Button buttonSignIn;
-    private Button googleSignIn;
+    private SignInButton googleSignIn;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog signInProgress;
     private GoogleSignInClient mGoogleSignInClient;
@@ -107,16 +108,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         buttonSignIn.setOnClickListener(this);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
 
 
 
     }
 
-    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build();
+
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -209,7 +214,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         //updateUI(currentUser);
     }
 
-    public void onClick(View view){
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.g_sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    /*public void onClick(View view){
         if(view == buttonSignIn){
             registerUser();
         }
@@ -217,7 +231,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent mainIntent = new Intent(this, MainActivity2.class);
             startActivity(mainIntent);
         }
-    }
+    }*/
 
 
 }
