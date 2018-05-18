aaec5e435a333021e6b5e8dbbc5f6d08b98a75b4
KianMderi <moghtaderik@gmail.com>
2018-05-18 12:02:51 -0700

    formatter 0.97

--- Scraper/TestFormatter.py
+++ Scraper/TestFormatter.py
@@ -1,13 +1,39 @@
 # -*- coding: cp1252 -*-
-#python class database creator yay!
 from __future__ import print_function
+
+
+with open('c:/Users/Kian/classlist.txt') as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+    for i in content:
+        fullname = i
+        j = i.split(' ',3)
+    
+        subject = j[0]
+        number = j[1]
+        #i[2] = '-'
+        section = j[3][0:2]
+        name = j[3][3:]
+        print ('\n"',end=""),
+        print (fullname[0:len(fullname)-1],end='"'),
+        print(' : { \n  '),
+        print('"department" : "',end=""),
+        print (subject,end=""),
+        print('",\n  "number" : "',end=""),
+        print (number,end=""),
+        print('",\n  "section" : "',end=""),
+        print (section,end=""),
+        print('",\n  "name" : "',end=""),
+        print (name[0:len(name)-1],end=""),
+        print('"\n  "students" : [{\n}]\n},',end="")
+        
+            
+
+#python class database creator yay!
+
 #take in string 'SUBJ ### - ## String of Name'
 #output 
-fullname = 'SUBJ 111 - 22 String of Name'
-subject = 'SUBJ'
-number = '111'
-section = '22'
-name = 'String of Name'
+
 #{
 #    " SUBJ ### - ## String of Name
 #
@@ -30,15 +56,4 @@ name = 'String of Name'
 #      }]
 #    },
 
-print ('\n"',end=""),
-print (fullname,end=""),
-print('" : { \n  "'),
-print('"department" : "',end=""),
-print (subject,end=""),
-print('",\n  "number" : "',end=""),
-print (number,end=""),
-print('",\n  "section" : "',end=""),
-print (section,end=""),
-print('",\n  "name" : "',end=""),
-print (name,end=""),
-print('"\n  "students" : [{\n}]\n},',end="")
+
